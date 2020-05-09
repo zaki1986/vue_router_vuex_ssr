58486a7b42fe33d8abd166898ff203ca1fdd7b1a
张闯/zhang-chuang <zhang_chuang@nhn-st.com>
2020-05-09 08:54:18 +0800

    生命周期

--- study/lifecycle/index.js
+++ study/lifecycle/index.js
@@ -6,25 +6,36 @@ new Vue({
     text: 'aaa'
   },
   beforeCreate () {
-    console.log(this.text, 'beforeCreate')
+    console.log(this.$el, 'beforeCreate') // undefined "beforeCreate"
   },
   created () {
-    console.log(this.text, 'created')
+    console.log(this.$el, 'created') // undefined "created"
   },
   beforeMount () {
-    console.log(this.text, 'beforeMount')
+    console.log(this.$el, 'beforeMount') // <div id="root"></div> "beforeMount"
   },
   mounted () {
-    console.log(this.text, 'mounted')
+    console.log(this.$el, 'mounted') // <div>aaa</div> "mounted"
   },
   beforeUpdate () {
-    console.log(this.text, 'beforeUpdate')
+    console.log(this.$el, 'beforeUpdate')
   },
   updated () {
-    console.log(this.text, 'updated')
+    console.log(this.$el, 'updated')
   },
   activated () {
-    console.log(this.text, 'activated')
+    console.log(this.$el, 'activated')
   },
-  template: '<div>{{text}}</div>'
+  // template: '<div>{{text}}</div>',
+  render (h) { // 如果没有定义template，就会执行render中的DOM内容
+    // throw new TypeError('render error')
+    console.log('this is render') // 出现在beforeMount和mounted之间
+    return h('div', {}, this.text)
+  },
+  renderError (h, err) {
+    return h('div', {}, err.stack)
+  },
+  errorCaptured () {
+    // 会向上冒泡，并且正式环境可以使用
+  }
 })
